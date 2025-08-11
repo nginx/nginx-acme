32636d0b6f221e274cdec83269bd93d80052c6b0
Aleksei Bavshin <a.bavshin@f5.com>
2025-08-11 14:25:47 -0700

    ACME: tls-alpn-01 challenge solver implementation.

--- build.rs
+++ build.rs
@@ -54,12 +54,17 @@ fn detect_nginx_features() {
     // Generate cfg values for version checks
 
     println!("cargo::rustc-check-cfg=cfg(ngx_ssl_cache)");
+    println!("cargo::rustc-check-cfg=cfg(ngx_ssl_client_hello_cb)");
     println!("cargo::rerun-if-env-changed=DEP_NGINX_VERSION_NUMBER");
     if let Ok(version) = env::var("DEP_NGINX_VERSION_NUMBER") {
         let version: u64 = version.parse().unwrap();
 
         if version >= 1_027_002 {
             println!("cargo::rustc-cfg=ngx_ssl_cache");
+        }
+
+        if version >= 1_029_002 {
+            println!("cargo::rustc-cfg=ngx_ssl_client_hello_cb");
         }
     }
 }
